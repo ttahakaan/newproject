258ec8015d9f47532dc22ef9dcab55ee07830e96
Taha Kaan Kutal <tkaanbs@outlook.com>
2023-10-23 15:50:54 +0300

    fixed a to link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,22 @@
+import Link from "next/link";
+
 export default function YeniProject() {
   return (
     <div className="bg-sky-100 h-screen overflow-scroll">
       <div className="bg-sky-400 h-16 text-white items-center flex justify-center text-lg rounded-b-md">Welcome!</div>
       <div class="grid grid-rows-2 grid-cols-2 h-screen p-4">
-        <a className="bg-sky-400 mr-2 p-4 items-center flex justify-center border border-gray-400 rounded-full text-white font-bold" href="/how-to-take-care-of-your-kalimba">
+        <Link className="bg-sky-400 mr-2 p-4 items-center flex justify-center border border-gray-400 rounded-full text-white font-bold" href="/how-to-take-care-of-your-kalimba">
           How to take care of your Kalimba?
-        </a>
-        <a className="bg-sky-400 p-4 items-center flex justify-center border border-gray-400 rounded-full text-white font-bold" href="/how-to-tune-your-kalimba">
+        </Link>
+        <Link className="bg-sky-400 p-4 items-center flex justify-center border border-gray-400 rounded-full text-white font-bold" href="/how-to-tune-your-kalimba">
           How to Tune Your Kalimba?
-        </a>
-        <a className="bg-sky-400 mr-2 mt-2 p-4 items-center flex justify-center border border-gray-400 rounded-full text-white font-bold " href="/how-to-make-your-own-kalimba-arrangement">
+        </Link>
+        <Link className="bg-sky-400 mr-2 mt-2 p-4 items-center flex justify-center border border-gray-400 rounded-full text-white font-bold " href="/how-to-make-your-own-kalimba-arrangement">
           How To Make You Own Kalimba Arrangement?
-        </a>
-        <a className="bg-sky-400 p-4 mt-2 items-center flex justify-center border border-gray-400 rounded-full text-white font-bold" href="/how-to-fix-buzzing-kalimba-tines">
+        </Link>
+        <Link className="bg-sky-400 p-4 mt-2 items-center flex justify-center border border-gray-400 rounded-full text-white font-bold" href="/how-to-fix-buzzing-kalimba-tines">
           How To Fix Buzzing Kalimba Tiness
-        </a>
+        </Link>
       </div>
     </div>
   );
